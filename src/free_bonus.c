d2c5df690b91dd69cd89006aecc108f0549ef1b0
Krystian Pawlowski <kpawlows@c2r6s1.42lausanne.ch>
2023-03-09 09:49:29 +0100

    noleak

--- src/free_bonus.c
+++ src/free_bonus.c
@@ -6,28 +6,28 @@
 /*   By: kpawlows <kpawlows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 18:57:02 by kpawlows          #+#    #+#             */
-/*   Updated: 2023/03/08 19:39:13 by kpawlows         ###   ########.fr       */
+/*   Updated: 2023/03/09 09:47:13 by kpawlows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex_bonus.h"
-
-void	free_commands(char *cmd, char **tmp)
-{
-	if (cmd != NULL)
-		free(cmd);
-	free_pointer_array(tmp);
-}
 
 void	free_pointer_array(char **s)
 {
 	int	i;
 
 	i = -1;
-	while (s[i++] != NULL)
+	while (s[++i] != NULL)
 		free(s[i]);
 	if (s != NULL)
 		free(s);
+}
+
+void	free_commands(char *cmd, char **tmp)
+{
+	if (cmd != NULL)
+		free(cmd);
+	free_pointer_array(tmp);
 }
 
 void	free_pipes(t_data *data)
